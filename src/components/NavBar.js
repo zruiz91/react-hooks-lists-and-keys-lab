9fc3c0b52e125ef2da3d7e57ec51df57aa76b52c
Zach Ruiz <powersbooth10@gmail.com>
2022-09-23 12:52:50 -0500

    commit tree 2194833943c639aa393e2d9415715d1c551d0ffa

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,10 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const anchors = links.map((link) => {
+    return <a key ={link} href={"#" + link}>{link}</a>
+  })
+  return <nav>{anchors}</nav>;
 }
 
 export default NavBar;
